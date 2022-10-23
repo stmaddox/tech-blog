86fd64dce0874b7d6ce2398ddeccddee626c9de1
Sean M <sean.maddox89@yahoo.com>
2022-10-22 23:18:07 -0700

    post-routes complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -56,3 +56,60 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post("/", withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    post_url: req.post.url,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+    },
+    {
+      where: req.params.id,
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id!" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  console.log("id", req.params.id);
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id!" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
